a42e71fadc283e04a50ca7507df73464f6a65071
Peter Berck <peter.berck@humlab.lu.se>
2023-11-28 10:45:26 +0100

    Changed if/then to more rust like unwrap_or().

--- src/main.rs
+++ src/main.rs
@@ -55,11 +55,8 @@ fn get_files_in_directory<P: AsRef<Path>>(path: P) -> Result<Vec<PathBuf>> {
     let mut file_paths = Vec::new();
 
     let args = Args::parse();
-    let mut counter:usize = 0;
-    if let Some(count) = args.maxfiles {
-        counter = count;
-    }
-    
+    let mut counter: usize = args.maxfiles.unwrap_or(0);
+
     for entry in fs::read_dir(path)? {
         let entry = entry?;
         let path = entry.path();
